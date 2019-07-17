4b56d2d62b6e487df8600d95cd767498aa728be2
Jez Higgins <jez@jezuk.co.uk>
2019-07-17 22:12:28 +0100

    linecount implementation

--- c++/chapter_1/3_linecount/linecount.cpp
+++ c++/chapter_1/3_linecount/linecount.cpp
@@ -1,8 +1,13 @@
 
 #include "linecount.h"
+#include <iostream>
 
 namespace stiX {
     size_t linecount(std::istream &in) {
-        return 0;
+        return std::count_if(
+                std::istreambuf_iterator<char>(in),
+                std::istreambuf_iterator<char>(),
+                [](char c) { return c == '\n'; }
+        );
     }
 }
