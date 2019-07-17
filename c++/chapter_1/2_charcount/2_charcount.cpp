72547b0e9b86b0798a7eb1bc2a42d014acfa6663
Jez Higgins <jez@jezuk.co.uk>
2019-07-17 21:12:47 +0100

    charcount executable

---
+++ c++/chapter_1/2_charcount/2_charcount.cpp
@@ -0,0 +1,18 @@
+//
+// charcount
+//
+// charcount counts the characters in its input and writes the total as a
+// single line of text to the output. Since each line of text is internally
+// delimited by a NEWLINE character, the total count is the number of lines
+// plus the number of characters within each line.
+//
+
+#include "charcount.h"
+#include <iostream>
+
+int main() {
+    std::cout << stiX::charcount(std::cin) << std::endl;
+}
+
+
+
