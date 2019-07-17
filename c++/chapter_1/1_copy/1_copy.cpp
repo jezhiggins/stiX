056e19d575a6eb4af0a7830a0293e5b832e9d6f6
Jez Higgins <jez@jezuk.co.uk>
2019-07-17 21:13:05 +0100

    typo fix

--- c++/chapter_1/1_copy/1_copy.cpp
+++ c++/chapter_1/1_copy/1_copy.cpp
@@ -3,7 +3,7 @@
 //
 // copy copies its input to its output unchanged. It is useful for copy from a
 // terminal to a file, or even from a terminal to a terminal. It may be used for
-// displaying the contents of a file, without interpretation or formating, by
+// displaying the contents of a file, without interpretation or formatting, by
 // copying from a file to a terminal.
 //
 
