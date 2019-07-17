47399ce75f5d56f6a93c274b630a990bf325f27e
Jez Higgins <jez@jezuk.co.uk>
2019-07-17 22:26:50 +0100

    linecount executable

---
+++ c++/chapter_1/3_linecount/3_linecount.cpp
@@ -0,0 +1,13 @@
+//
+// linecount
+//
+// linecount counts the lines in its input and writes the total
+// as line of text to the output
+//
+
+#include "linecount.h"
+#include <iostream>
+
+int main() {
+    std::cout << stiX::linecount(std::cin) << std::endl;
+}
