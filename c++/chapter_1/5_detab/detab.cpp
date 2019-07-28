f7889d237dcf42a27d08ad0fe562cf6d54395fe1
Jez Higgins <jez@jezuk.co.uk>
2019-07-28 21:38:23 +0100

    detab - hoist lambda passthough into a function object

--- c++/chapter_1/5_detab/detab.cpp
+++ c++/chapter_1/5_detab/detab.cpp
@@ -3,13 +3,19 @@
 
 #include "detab.h"
 
+struct detabber {
+    char operator()(char c) {
+        return c;
+    }
+};
+
 namespace stiX {
     void detab(std::istream &in, std::ostream &out) {
         std::transform(
                 std::istreambuf_iterator<char>(in),
                 std::istreambuf_iterator<char>(),
                 std::ostreambuf_iterator<char>(out),
-                [](char c) { return c; }
+                detabber()
         );
     }
 }
