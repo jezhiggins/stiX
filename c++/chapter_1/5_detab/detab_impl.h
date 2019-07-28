4fa18e3f03c7b249a88d3f36d7355d722d29972f
Jez Higgins <jez@jezuk.co.uk>
2019-07-28 22:04:31 +0100

    detab - add tabSize as a global constant
    see https://www.fluentcpp.com/2019/07/23/how-to-define-a-global-constant-in-cpp/

--- c++/chapter_1/5_detab/detab_impl.h
+++ c++/chapter_1/5_detab/detab_impl.h
@@ -2,6 +2,8 @@
 #define STICPP_DETAB_IMPL_H
 
 namespace stiX {
+    inline const size_t tabSize = 8;
+
     size_t next_tab_stop(size_t position) {
         return 0;
     }
--- c++/chapter_1/5_detab/test_detabber.cpp
+++ c++/chapter_1/5_detab/test_detabber.cpp
@@ -5,6 +5,6 @@
 
 TEST_CASE("Chapter 1 - detab - detabber", "next_tab_position") {
     for (size_t p = 0; p != 8; ++p)
-        REQUIRE(stiX::next_tab_stop(p) == 8);
+        REQUIRE(stiX::next_tab_stop(p) == stiX::tabSize);
 
 }
