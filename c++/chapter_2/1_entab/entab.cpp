100bb3d578dce141fec024cb8ed05cc8539e0c90
Jez Higgins <jez@jezuk.co.uk>
2019-08-08 21:24:37 +0100

    pass through

--- c++/chapter_2/1_entab/entab.cpp
+++ c++/chapter_2/1_entab/entab.cpp
@@ -1,8 +1,24 @@
 #include "entab.h"
 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <string>
+
+struct entabber {
+    std::string operator()(char c) {
+        return std::string(1, c);
+    }
+};
+
 namespace stiX {
     void entab(std::istream &in, std::ostream &out) {
-
+        std::transform(
+                std::istreambuf_iterator<char>(in),
+                std::istreambuf_iterator<char>(),
+                std::ostream_iterator<std::string>(out),
+                entabber()
+        );
     }
 }
 
