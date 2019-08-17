bdfaafecd1c2b8efdad9bc9d28bbd26cdc66c711
Jez Higgins <jez@jezuk.co.uk>
2019-08-17 09:49:30 +0100

    Do nothing compress stub implementation

--- c++/chapter_2/3_compress/compress.cpp
+++ c++/chapter_2/3_compress/compress.cpp
@@ -1,10 +1,15 @@
 #include "compress.h"
-#include <iostream>
-#include <iterator>
-#include <algorithm>
+#include "../../lib/filter.h"
+
+struct compressor {
+    std::string operator()(char c) {
+      return std::string(1, c);
+    }
+};
 
 namespace stiX {
     void compress(std::istream &in, std::ostream &out) {
+        filter(in, out, compressor());
     }
 }
 
