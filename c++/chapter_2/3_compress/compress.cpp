b4001e9b6c6cb446203b2aeaf10fc049af502d30
Jez Higgins <jez@jezuk.co.uk>
2019-08-17 21:56:43 +0100

    first cut at compress

--- c++/chapter_2/3_compress/compress.cpp
+++ c++/chapter_2/3_compress/compress.cpp
@@ -2,8 +2,37 @@
 #include "../../lib/filter.h"
 
 struct compressor {
+    std::string const countEncoding =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string const empty;
+
+    char lastChar = 0;
+    size_t repeat = 0;
+    std::string output;
+
     std::string operator()(char c) {
-      return std::string(1, c);
+        if (lastChar == c) {
+            ++repeat;
+            return empty;
+        }
+
+        output = empty;
+
+        if (repeat) {
+            if (repeat >= 3) {
+                output += '~';
+                output += countEncoding[repeat];
+            } else {
+                while (repeat-- > 0)
+                    output += lastChar;
+            }
+            repeat = 0;
+        }
+
+        output += c;
+        lastChar = c;
+
+        return output;
     }
 };
 
