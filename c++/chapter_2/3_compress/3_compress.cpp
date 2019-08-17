276981f13107c909ce871940f9a20dae93f5f5f4
Jez Higgins <jez@jezuk.co.uk>
2019-08-17 23:58:29 +0100

    having added the description of compress, I didn't read it properly the first time round, and so all my tests are wrong. Consequently my implementation is wrong too.

--- c++/chapter_2/3_compress/3_compress.cpp
+++ c++/chapter_2/3_compress/3_compress.cpp
@@ -1,3 +1,15 @@
+//
+// compress
+//
+// compress copies its input to its output, replacing
+// strings of four or more identical characters by a code
+// sequence so that the output generally contains fewer
+// characters than the input. A run of x's is encoded as ~nx,
+// where the count n is a character: 'A' calls for a
+// repetition of one x, 'B" a repetition of two x's, and so
+// on. Runs longer than 26 are broken into several shorter
+// ones. Runs of ~'s of any length are encoded.
+
 #include "compress.h"
 #include <iostream>
 
