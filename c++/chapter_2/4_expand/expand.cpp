d6411e6aa0210e7c03899d22fac96cdcbe105ee6
Jez Higgins <jez@jezuk.co.uk>
2019-08-22 09:06:54 +0100

    initial implementation

--- c++/chapter_2/4_expand/expand.cpp
+++ c++/chapter_2/4_expand/expand.cpp
@@ -1,7 +1,50 @@
 #include "expand.h"
 #include "../../lib/filter.h"
 
+enum ExpandState {
+    passthrough,
+    haveMarker,
+    haveCount
+};
+
+class expander {
+public: 
+    std::string operator()(char c) {
+        switch (state) {
+            case haveMarker:
+                state = haveCount;
+                count = expandCount(c);
+                return empty;
+            case haveCount:
+                state = passthrough;
+                return std::string(count, c);
+            case passthrough:
+                if (c == '~') {
+                    state = haveMarker;
+                    return empty;
+                }
+                return std::string(1, c);
+        }
+    }
+
+private:
+    size_t expandCount(char c) {
+        auto position = countEncoding.find(c);
+        return position != std::string::npos
+            ? position + 1
+            : std::string::npos;
+    }
+
+    std::string const countEncoding =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string const empty;
+
+    ExpandState state = passthrough;
+    size_t count;
+};
+
 namespace stiX {
     void expand(std::istream& in, std::ostream& out) {
+        filter(in, out, expander());
     }
 }
