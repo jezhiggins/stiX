82c0e404d047ee8b816cadedcbbdcb6dbb20804a
Jez Higgins <jez@jezuk.co.uk>
2019-09-06 22:47:52 +0100

    expand implementation

--- c++/chapter_2/4_expand/expand.cpp
+++ c++/chapter_2/4_expand/expand.cpp
@@ -1,3 +1,4 @@
+#include <optional>
 #include "expand.h"
 #include "../../lib/filter.h"
 
@@ -8,18 +9,35 @@ enum ExpandState {
 };
 
 class expander {
-public: 
+public:
+    expander(std::ostream& out)
+      : out_(out) {
+    }
+    ~expander() {
+      if (state == passthrough)
+        return;
+
+      out_ << recoveryString;
+      if (state == haveCount)
+        out_ << lastChar;
+    }
+
     std::string operator()(char c) {
+        lastChar = c;
+
         switch (state) {
-            case haveMarker:
-                state = haveCount;
-                count = expandCount(c);
-                return empty;
+            case haveMarker: {
+                    count = decodeCount(c);
+                    if (count == std::string::npos)
+                        return recover(c);
+                    state = haveCount;
+                    return empty;
+                }
             case haveCount:
                 state = passthrough;
                 return std::string(count, c);
             case passthrough:
-                if (c == '~') {
+                if (ismarker(c)) {
                     state = haveMarker;
                     return empty;
                 }
@@ -28,23 +46,37 @@ public:
     }
 
 private:
-    size_t expandCount(char c) {
+    bool ismarker(char c) { return c == '~'; }
+
+    size_t decodeCount(char c) {
         auto position = countEncoding.find(c);
         return position != std::string::npos
             ? position + 1
             : std::string::npos;
     }
 
+    std::string recover(char c) {
+        if (ismarker(c)) // repeated marker
+          return recoveryString;
+
+        // bad count
+        state = passthrough;
+        return recoveryString + c;
+    }
+
+    std::ostream& out_;
     ExpandState state = passthrough;
     size_t count;
+    char lastChar;
 
     std::string const countEncoding =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string const recoveryString = "~";
     std::string const empty;
 };
 
 namespace stiX {
     void expand(std::istream& in, std::ostream& out) {
-        filter(in, out, expander());
+        filter(in, out, expander(out));
     }
 }
