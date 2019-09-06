2be30a0563df10bdfb17055858c5959798ae0a3a
Jez Higgins <jez@jezuk.co.uk>
2019-09-06 21:09:01 +0100

    filled out echo tests

--- c++/chapter_2/5_echo/test.cpp
+++ c++/chapter_2/5_echo/test.cpp
@@ -3,24 +3,52 @@
 #include "echo.h"
 
 char const* argv[] = {
+    "fruit",
     "apple",
     "banana",
     "plum"
 };
 
-void testEcho(int n, std::string expected);
+void testEcho(int n);
 
 TEST_CASE("Chapter 2 - echo") {
-    testEcho(0, "");
-    testEcho(1, "apple");
+    testEcho(0);
+    testEcho(1);
+    testEcho(2);
+    testEcho(3);
 }
 
-void testEcho(int n, std::string expected) {
-  DYNAMIC_SECTION("echo(" << n << ")") {
+std::string testLabel(int n);
+std::string expected(int n);
+
+void testEcho(int n) {
+  DYNAMIC_SECTION("echo(" << testLabel(n) << ")") {
     std::ostringstream os;
 
     stiX::echo(n, argv, os);
 
-    REQUIRE(os.str() == expected);
+    REQUIRE(os.str() == expected(n));
   }
+}
+
+std::string testLabel(int n) {
+  std::ostringstream l;
+  l << '[';
+  for (int i = 0; i != n; ++i) {
+      if (i > 0)
+        l << ',';
+      l << argv[i+1];
+  }
+  l << ']';
+  return l.str();
+}
+
+std::string expected(int n) {
+  std::ostringstream e;
+  for (int i = 0; i != n; ++i) {
+      if (i > 0)
+        e << ' ';
+      e << argv[i+1];
+  }
+  return e.str();
 }
