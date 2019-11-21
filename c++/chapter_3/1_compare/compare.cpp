70713362d9ccb3b5a0ca2c0d67027a3f4d0c5ed5
Jez Higgins <jez@jezuk.co.uk>
2019-11-21 22:31:19 +0000

    Filled out 1_compare

--- c++/chapter_3/1_compare/compare.cpp
+++ c++/chapter_3/1_compare/compare.cpp
@@ -1,5 +1,10 @@
 
 #include "compare.hpp"
+#include <iostream>
+
+namespace {
+    const std::string end_of_file = "end of file on ";
+}
 
 namespace stiX {
     void compare(
@@ -9,6 +14,32 @@ namespace stiX {
         std::istream& right,
         std::ostream& out
     ) {
+        size_t line = 1;
+        while(true) {
+            std::string leftline;
+            std::string rightline;
 
+            std::getline(left, leftline);
+            std::getline(right, rightline);
+
+            if (left.eof() && right.eof())
+                return;
+            if (left.eof() && !right.eof()) {
+                out << end_of_file << left_file_name << '\n';
+                return;
+            }
+            if (!left.eof() && right.eof()) {
+                out << end_of_file << right_file_name << '\n';
+                return;
+            }
+
+            if (leftline != rightline) {
+                out << line << ':' << '\n'
+                    << leftline << '\n'
+                    << rightline << '\n';
+            }
+
+            ++line;
+        }
     } // compare
 }
--- c++/chapter_3/1_compare/test.cpp
+++ c++/chapter_3/1_compare/test.cpp
@@ -8,27 +8,27 @@ void testCompare(
     std::string expectedOutput
 );
 
-const std::string mary_had_a_little_lamb_its_fleece_was_white = R"c(
-Mary had a little lamb
+const std::string mary_had_a_little_lamb_its_fleece_was_white =
+R"c(Mary had a little lamb
 Its fleece was white as snow
 And everywhere that Mary went
 The lamb was sure to go
 )c";
 
-const std::string mary_had_a_little_lamb_its_fleece_was_kind_of_creamy = R"c(
-Mary had a little lamb
+const std::string mary_had_a_little_lamb_its_fleece_was_kind_of_creamy =
+R"c(Mary had a little lamb
 Its fleece was a sort of porridge colour
 And everywhere that Mary went
 The lamb was sure to go
 )c";
 
-const std::string mary_had_a_little_lamb = R"c(
-Mary had a little lamb
+const std::string mary_had_a_little_lamb =
+R"c(Mary had a little lamb
 Its fleece was white as snow
 )c";
 
-const std::string santa_claus_is_coming_to_town = R"c(
-You better watch out
+const std::string santa_claus_is_coming_to_town =
+R"c(You better watch out
 You better not cry
 You better not pout
 I'm telling you why
@@ -46,22 +46,21 @@ TEST_CASE("Chapter 3 - compare") {
         testCompare(
             mary_had_a_little_lamb,
             mary_had_a_little_lamb_its_fleece_was_white,
-            "end of file on left"
+            "end of file on left\n"
         );
     }
     SECTION("right is prefix") {
         testCompare(
             mary_had_a_little_lamb_its_fleece_was_white,
             mary_had_a_little_lamb,
-            "end of file on right"
+            "end of file on right\n"
         );
     }
     SECTION("contents are slightly different") {
         testCompare(
             mary_had_a_little_lamb_its_fleece_was_white,
             mary_had_a_little_lamb_its_fleece_was_kind_of_creamy,
-        R"c(
-2:
+        R"c(2:
 Its fleece was white as snow
 Its fleece was a sort of porridge colour
 )c"
@@ -71,8 +70,7 @@ Its fleece was a sort of porridge colour
         testCompare(
             mary_had_a_little_lamb_its_fleece_was_white,
             santa_claus_is_coming_to_town,
-        R"c(
-1:
+        R"c(1:
 Mary had a little lamb
 You better watch out
 2:
