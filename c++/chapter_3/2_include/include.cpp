9cfd333e4b92806525daa0b7cdab3f0701541bfc
Jez Higgins <jez@jezuk.co.uk>
2019-12-09 21:44:05 +0000

    first pass at actually including

--- c++/chapter_3/2_include/include.cpp
+++ c++/chapter_3/2_include/include.cpp
@@ -6,4 +6,14 @@ namespace stiX {
         std::getline(input, line);
         return line;
     } // getline
+
+    std::string extract_included(std::string include_line)
+    {
+        size_t first_quote = stiX::hash_include.size();
+        size_t second_quote = include_line.find('"', first_quote+1);
+        return include_line.substr(
+            first_quote,
+            second_quote - first_quote
+        );
+    }
 }
--- c++/chapter_3/2_include/include.hpp
+++ c++/chapter_3/2_include/include.hpp
@@ -3,9 +3,13 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 namespace stiX {
     std::string getline(std::istream& input);
+    std::string extract_included(std::string include_line);
+
+    const std::string hash_include("#include \"");
 
     template<typename IncludeOpener>
     void expand_include(
@@ -16,7 +20,14 @@ namespace stiX {
         while(input) {
             std::string line = getline(input);
 
-            output << line << (!input.eof() ? "\n" : "");
+            if (line.find(hash_include) == std::string::npos) {
+                output << line
+                       << (!input.eof() ? "\n" : "");
+            } else {
+                auto name = extract_included(line);
+                auto included = openFn(name);
+                expand_include(included, output, openFn);
+            }
         } // while
     } // include
 }
