5928b1fcdee7946800c7dcad23ea8580ed326b57
Jez Higgins <jez@jezuk.co.uk>
2020-01-13 22:16:28 +0000

    add nested include and bad include tests

--- c++/chapter_3/2_include/test.cpp
+++ c++/chapter_3/2_include/test.cpp
@@ -5,7 +5,8 @@
 
 void testInclude(
     std::string input,
-    std::string expectedOutput
+    std::string expectedOutput,
+    bool good = true
 );
 
 const std::string mary_had_a_little_lamb =
@@ -23,6 +24,21 @@ const std::string line_with_newline =
 const std::string include_mary =
     "#include \"mary\"";
 
+const std::string include_include_mary =
+    "#include \"include_mary\"";
+
+const std::string bad_include =
+R"c(Here is a line
+#include "missing"
+And another line
+)c";
+
+const std::string expanded_bad_include =
+R"c(Here is a line
+ERROR: Could not include "missing"
+)c";
+
+
 TEST_CASE("Chapter 3 - include") {
     SECTION("no include directive") {
         for (auto l : { line_with_newline, line_without_newline, mary_had_a_little_lamb })
@@ -39,26 +55,48 @@ TEST_CASE("Chapter 3 - include") {
         );
     }
 
+    SECTION("nested-include directives") {
+        testInclude(
+            include_include_mary,
+            mary_had_a_little_lamb
+        );
+    }
+
+    SECTION("include non-existant file") {
+        testInclude(
+            bad_include,
+            expanded_bad_include,
+            false
+        );
+    }
+
 } // TEST_CASE
 
 std::istringstream testIncludeOpener(std::string openname) {
-    std::string output = "error";
+    std::string output;
     if (openname == "mary")
         output = mary_had_a_little_lamb;
-    return std::istringstream(mary_had_a_little_lamb);
+    else if (openname == "include_mary")
+        output = include_mary;
+    auto ss = std::istringstream(output);
+    if (output.empty())
+        ss.setstate(std::ios_base::badbit);
+    return ss;
 }
 
 void testInclude(
     std::string input,
-    std::string expected
+    std::string expected,
+    bool expectedGood
 ) {
     std::istringstream inputs(input);
     std::ostringstream output;
 
-    stiX::expand_include(
+    bool ok = stiX::expand_include(
         inputs,
         output,
         testIncludeOpener
     );
     REQUIRE(output.str() == expected);
+    REQUIRE(ok == expectedGood);
 }
