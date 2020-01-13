ebc5c4fa1def94c422dae81e2d274483596e0d99
Jez Higgins <jez@jezuk.co.uk>
2020-01-13 22:19:32 +0000

    handle missing include and report error

--- c++/chapter_3/2_include/include.hpp
+++ c++/chapter_3/2_include/include.hpp
@@ -11,24 +11,53 @@ namespace stiX {
     std::string extract_included(const std::string& include_line);
     std::ifstream file_opener(const std::string& filename);
 
+    template<typename IncludeOpener>
+    decltype(auto) open_include(
+        const std::string& include_line,
+        std::ostream& output,
+        IncludeOpener openFn
+    ) {
+        auto name = extract_included(include_line);
+        auto included = openFn(name);
+        if (!included) {
+            output
+                << "ERROR: Could not include \""
+                << name
+                << "\"\n";
+        }
+        return included;
+    }
+
     template<typename IncludeOpener = typeof(file_opener)>
-    void expand_include(
+    bool expand_include(
         std::istream& input,
         std::ostream& output,
         IncludeOpener openFn = file_opener
     ) {
-        while(input) {
+        bool good = true;
+        while(input && good) {
             std::string line = getline(input);
 
             if (!is_include(line)) {
                 output << line
                        << (!input.eof() ? "\n" : "");
             } else {
-                auto name = extract_included(line);
-                auto included = openFn(name);
-                expand_include(included, output, openFn);
+                auto included = open_include(
+                    line,
+                    output,
+                    openFn
+                );
+                if (!included)
+                    return false;
+
+                good = expand_include(
+                    included,
+                    output,
+                    openFn
+                );
             }
         } // while
+        return good;
     } // include
 }
 #endif //STICPP_INCLUDE_HPP
