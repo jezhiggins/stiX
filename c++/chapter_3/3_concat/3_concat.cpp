6a98100965f649d0fed86808f490df75ceb7d96c
Jez Higgins <jez@jezuk.co.uk>
2020-01-16 16:06:41 +0000

    Chapter 3 concat

--- c++/chapter_3/3_concat/3_concat.cpp
+++ c++/chapter_3/3_concat/3_concat.cpp
@@ -8,3 +8,27 @@
 // larger file. Since concat performs no reformatting or
 // interpretation of the input files, it is useful for
 // displaying the contents of a file
+
+#include <iostream>
+#include <fstream>
+#include "../../lib/arguments.h"
+
+std::ifstream openfile(std::string filename);
+
+int main(int argc, char const* argv[]) {
+  auto filenames = stiX::make_arguments(argc, argv);
+
+  for (auto filename : filenames) {
+    std::ifstream file(filename);
+    if (!file) {
+      std::cerr << "Could not open '" << filename << "'\n";
+      continue;
+    }
+
+    std::copy(
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>(),
+        std::ostreambuf_iterator<char>(std::cout)
+    );
+  }
+}
