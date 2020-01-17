8979262709c4a1ed19266632a9d78a9fd1130fe6
Jez Higgins <jez@jezuk.co.uk>
2020-01-17 21:52:54 +0000

    makecopy

--- c++/chapter_3/5_makecopy/5_makecopy.cpp
+++ c++/chapter_3/5_makecopy/5_makecopy.cpp
@@ -8,3 +8,34 @@
 // rewritten, otherwise it is made to exist. The new file
 // is an exact replica of the old.
 
+#include <iostream>
+#include <filesystem>
+#include "../../lib/arguments.h"
+
+namespace fs = std::filesystem;
+
+int main(int argc, char const* argv[]) {
+  auto filenames = stiX::make_arguments(argc, argv);
+
+  if (filenames.size() != 2) {
+    std::cerr << "Error: makecopy old new\n";
+    return 0;
+  }
+
+  auto source = fs::path(filenames[0]);
+  auto destination = fs::path(filenames[1]);
+
+  if (fs::equivalent(source, destination)) {
+    std::cerr << "Error: source and destination are the same file\n";
+    return 0;
+  }
+
+  try {
+    if (fs::exists(destination) && fs::is_regular_file(destination))
+      fs::remove(destination);
+
+    fs::copy_file(source, destination);
+  } catch (const fs::filesystem_error& fse) {
+    std::cerr << fse.what() << '\n';
+  }
+}
