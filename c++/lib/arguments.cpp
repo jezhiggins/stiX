c95f0a656a723d8eaf01cdae65564b151c934e5f
Jez Higgins <jez@jezuk.co.uk>
2020-04-14 16:04:08 +0100

    properly push make_arguments into the stix library

---
+++ c++/lib/arguments.cpp
@@ -0,0 +1,11 @@
+#include "arguments.h"
+
+namespace stiX {
+    std::vector<std::string> make_arguments(int argc, char const* argv[]) {
+      std::vector<std::string> args;
+      for (int a = 1; a != argc; ++a)
+        args.emplace_back(argv[a]);
+      return args;
+    }
+}
+
--- c++/lib/arguments.h
+++ c++/lib/arguments.h
@@ -5,12 +5,6 @@
 #include <vector>
 
 namespace stiX {
-    std::vector<std::string> make_arguments(int argc, char const* argv[]) {
-        std::vector<std::string> args;
-        for (int a = 1; a != argc; ++a)
-            args.emplace_back(argv[a]);
-        return args;
-    }
+    std::vector<std::string> make_arguments(int argc, char const* argv[]);
 }
-
 #endif
