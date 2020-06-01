3d32c4dfe14d82f2648277dae6dce13fbeceb1aa
Jez Higgins <jez@jezuk.co.uk>
2020-06-01 11:30:20 +0100

    5_makecopy - pull out path checking into its own function

--- c++/chapter_3/5_makecopy/5_makecopy.cpp
+++ c++/chapter_3/5_makecopy/5_makecopy.cpp
@@ -10,32 +10,37 @@
 
 #include <iostream>
 #include <filesystem>
+#include <tuple>
 #include "../../lib/arguments.hpp"
 
 namespace fs = std::filesystem;
 
+std::tuple<fs::path, fs::path> file_paths(int argc, char const* argv[]);
+
 int main(int argc, char const* argv[]) {
-  auto filenames = stiX::make_arguments(argc, argv);
-
-  if (filenames.size() != 2) {
-    std::cerr << "Error: makecopy old new\n";
-    return 0;
-  }
-
-  auto source = fs::path(filenames[0]);
-  auto destination = fs::path(filenames[1]);
-
-  if (fs::equivalent(source, destination)) {
-    std::cerr << "Error: source and destination are the same file\n";
-    return 0;
-  }
-
   try {
+    auto [source, destination] = file_paths(argc, argv);
+
     if (fs::exists(destination) && fs::is_regular_file(destination))
       fs::remove(destination);
 
     fs::copy_file(source, destination);
-  } catch (const fs::filesystem_error& fse) {
+  } catch (const std::exception& fse) {
     std::cerr << fse.what() << '\n';
   }
 }
+
+std::tuple<fs::path, fs::path> file_paths(int argc, char const* argv[]) {
+  auto filenames = stiX::make_arguments(argc, argv);
+
+  if (filenames.size() != 2)
+    throw std::runtime_error("Error: makecopy old new");
+
+  auto source = fs::path(filenames[0]);
+  auto destination = fs::path(filenames[1]);
+
+  if (fs::equivalent(source, destination))
+    throw std::runtime_error("Error: source and destination are the same file");
+
+  return std::make_tuple(source, destination);
+} // file_paths
