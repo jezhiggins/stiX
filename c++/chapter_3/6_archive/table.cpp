09dc95a6d385935093807b13c0eb243ee9fdd693
Jez Higgins <jez@jezuk.co.uk>
2020-07-28 22:19:47 +0100

    Start of archive table of contents
    needs some tidying up - duplication to get rid off

---
+++ c++/chapter_3/6_archive/table.cpp
@@ -0,0 +1,40 @@
+#include "table.hpp"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include "../../lib/getline.hpp"
+
+namespace stiX {
+  struct archive_file {
+    std::string name;
+    size_t filesize;
+  };
+
+  archive_file parse_header(std::string const& header) {
+    if (header.find("-h- ") != 0)
+      throw std::logic_error("Not a header line");
+
+    auto lastspace = header.find_last_of(' ');
+    auto namelength = lastspace-4;
+    auto name = header.substr(4, namelength);
+    auto size = header.substr(lastspace+1);
+
+    return { name, std::stoul(size) };
+  } // parse_header
+
+  void table_archive(
+    std::istream& archive,
+    std::ostream& out
+  ) {
+    archive.peek();
+
+    while (archive && !archive.eof()) {
+      auto header_line = getline(archive);
+      auto header = parse_header(header_line);
+
+      out << header.name << '\n';
+
+      archive.peek();
+    }
+  } // table_archive
+}
---
+++ c++/chapter_3/6_archive/table.hpp
@@ -0,0 +1,13 @@
+#ifndef STICPP_TABLE_HPP
+#define STICPP_TABLE_HPP
+
+#include <iosfwd>
+
+namespace stiX {
+  void table_archive(
+    std::istream& archive,
+    std::ostream& out
+  );
+}
+
+#endif //STICPP_TABLE_HPP
