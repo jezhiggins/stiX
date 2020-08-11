f928dbbfb5ad30a9d53b122d6aadd4f3c67c2808
Jez Higgins <jez@jezuk.co.uk>
2020-08-11 11:35:05 +0100

    A bit of tidy up in the create tests

--- c++/chapter_3/6_archive/test.cpp
+++ c++/chapter_3/6_archive/test.cpp
@@ -2,6 +2,8 @@
 #include "../../testlib/testlib.h"
 #include "../../lib/file_open.hpp"
 
+namespace fs = std::filesystem;
+
 std::string asArchive(std::string const& name, std::string const& contents) {
   auto o = std::ostringstream();
 
@@ -25,6 +27,27 @@ auto const archiveTestFile3 = asArchive("testFile3", testFile3);
 
 auto const archiveWithContents =
   archiveTestFile1 + archiveTestFile2 + archiveTestFile3;
+
+class working_directory {
+public:
+  working_directory(std::string const& fixture) :
+    pwd_(fs::current_path()),
+    cwd_(fs::current_path() / "fixture" / fixture) {
+    fs::current_path(cwd_);
+  } // FileTestFixture
+
+  operator fs::path() const {
+    return cwd_;
+  } // operation fs::path
+
+  ~working_directory() {
+    fs::current_path(pwd_);
+  } // ~FileTestFixture
+
+private:
+  fs::path pwd_;
+  fs::path cwd_;
+}; // working_directory
 
 #include "test/test_create.inc"
 #include "test/test_table.inc"
