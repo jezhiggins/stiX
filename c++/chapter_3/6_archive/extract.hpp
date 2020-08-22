9d7e0698374c2e9ed4828c916f2e2a05a0dc0af8
Jez Higgins <jez@jezuk.co.uk>
2020-08-22 21:53:24 +0100

    extract implementation

--- c++/chapter_3/6_archive/extract.hpp
+++ c++/chapter_3/6_archive/extract.hpp
@@ -13,6 +13,20 @@ namespace stiX {
     std::vector<std::string> const& files,
     FileWriteOpener file_write_opener
   ) {
+    read_archive(
+      archive_in,
+      [&files, &file_write_opener](
+        std::istream& archive_in,
+        stiX::archive_file const& header
+      ) {
+        if (of_interest(files, header)) {
+          auto& out = file_write_opener(header.name);
+          copy_contents(archive_in, header, out);
+        }
+        else
+          skip_entry(archive_in, header);
+      }
+    );
   } // extract_archive
 }
 #endif //STICPP_EXTRACT_HPP
