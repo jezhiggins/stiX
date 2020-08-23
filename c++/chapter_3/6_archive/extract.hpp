c64383bf45f4af583532716a9f98a53a7987248c
Jez Higgins <jez@jezuk.co.uk>
2020-08-23 19:09:47 +0100

    Tweak out declaration to fell decltype of the FileWriteOpener. Means it can return reference or value without problem

--- c++/chapter_3/6_archive/extract.hpp
+++ c++/chapter_3/6_archive/extract.hpp
@@ -4,23 +4,24 @@
 #include <string>
 #include <vector>
 
+#include "../../lib/file_open.hpp"
 #include "./archive_file.hpp"
 
 namespace stiX {
-  template<typename FileWriteOpener>
+  template<typename FileWriteOpener = decltype(file_write_opener)>
   void extract_files(
     std::istream& archive_in,
     std::vector<std::string> const& files,
-    FileWriteOpener file_write_opener
+    FileWriteOpener file_writer = file_write_opener
   ) {
     read_archive(
       archive_in,
-      [&files, &file_write_opener](
+      [&files, &file_writer](
         std::istream& archive_in,
         stiX::archive_file const& header
       ) {
         if (of_interest(files, header)) {
-          auto& out = file_write_opener(header.name);
+          decltype(file_writer(header.name)) out = file_writer(header.name);
           copy_contents(archive_in, header, out);
         }
         else
