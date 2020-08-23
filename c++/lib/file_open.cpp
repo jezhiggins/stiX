96e4e9d49654ada588cc30a9f0ab81a7d7602954
Jez Higgins <jez@jezuk.co.uk>
2020-08-23 20:20:03 +0100

    file_write_opener creates directories

--- c++/lib/file_open.cpp
+++ c++/lib/file_open.cpp
@@ -1,4 +1,7 @@
 #include "file_open.hpp"
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace stiX {
     std::ifstream file_opener(std::string const& filename) {
@@ -6,6 +9,9 @@ namespace stiX {
     } // file_opener
 
     std::ofstream file_write_opener(std::string const& filename) {
-      return std::ofstream(filename);
+      auto path = fs::path(filename);
+      fs::create_directories(path.parent_path());
+
+      return std::ofstream(path);
     } // file_write_opener
 }
