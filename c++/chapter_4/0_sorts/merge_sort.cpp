dcea6c72c1e04358d1e6d59a5928af3b9b49eee2
Jez Higgins <jez@jezuk.co.uk>
2020-11-03 21:05:57 +0000

    starting merge_sort
    beginning with the insertion_sort part of the implementation - this is
    just do nothing skeletons so far

--- c++/chapter_4/0_sorts/merge_sort.cpp
+++ c++/chapter_4/0_sorts/merge_sort.cpp
@@ -1,4 +1,24 @@
-//
-// Created by jez on 28/10/2020.
-//
+#define CATCH_CONFIG_MAIN
+#include "../../testlib/catch.hpp"
 
+namespace stiX {
+  template<class Iterator, class Comparator = std::less<>>
+  void insertion_sort(Iterator begin, Iterator end, Comparator comparator = Comparator()) {
+
+  } // insertion_sort
+
+  template<class Container, class Comparator = std::less<>>
+  void insertion_sort(Container&& sample, Comparator comparator = Comparator()) {
+    insertion_sort(std::begin(sample), std::end(sample), comparator);
+  }
+}
+
+TEST_CASE("Chapter 4 - insertion sort") {
+  SECTION("sort { 1, 2 }") {
+    const auto expected = std::vector { 1, 2 };
+
+    auto sample = std::vector { 1, 2 };
+    stiX::insertion_sort(sample);
+    REQUIRE(sample == expected);
+  }
+}
