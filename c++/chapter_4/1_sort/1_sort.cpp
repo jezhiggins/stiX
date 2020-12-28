02ea287c8d05a82d337937e5dd176b82675d8bac
Jez Higgins <jez@jezuk.co.uk>
2020-12-28 12:51:00 +0000

    Completed in memory sort
    No tests for this because I'm just pluggin together standard
    components and if they're in the right order then it will just
    work

--- c++/chapter_4/1_sort/1_sort.cpp
+++ c++/chapter_4/1_sort/1_sort.cpp
@@ -4,17 +4,30 @@
 #include <iterator>
 #include "../../lib/getline.hpp"
 
+std::vector<std::string> read_lines();
+void write_lines(std::vector<std::string> const &lines);
+
 int main() {
+  auto lines = read_lines();
+
+  std::sort(lines.begin(), lines.end());
+
+  write_lines(lines);
+} // main
+
+std::vector<std::string> read_lines() {
   auto lines = std::vector<std::string> { };
 
   while (std::cin)
     lines.emplace_back(stiX::getline(std::cin));
 
-  std::sort(lines.begin(), lines.end());
+  return lines;
+} // read_lines
 
+void write_lines(std::vector<std::string> const &lines) {
   std::copy(
     lines.begin(),
     lines.end(),
     std::ostream_iterator<std::string>(std::cout, "\n")
   );
-}
+} // write_lines
