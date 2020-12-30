56124e401ed9be4bf45be9095df3b007f4e1f880
Jez Higgins <jez@jezuk.co.uk>
2020-12-30 22:17:47 +0000

    whammo in a load of tests. it does not sort after all :(

--- c++/chapter_4/0_sorts/quick_sort.cpp
+++ c++/chapter_4/0_sorts/quick_sort.cpp
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "../../testlib/catch.hpp"
 #include <iterator>
+#include <list>
 
 namespace stiX {
-  template<class Iterator>
-  void quick_sort(Iterator begin, Iterator end) {
+  template<class Iterator, class Comparator = std::less<>>
+  void quick_sort(Iterator begin, Iterator end, Comparator comparator = Comparator()) {
     if(std::distance(begin, end) <= 1)
       return;
 
@@ -13,10 +14,10 @@ namespace stiX {
     auto right = std::prev(end);
 
     while (left != right) {
-      while ((left != right) && (*left < pivot))
+      while ((left != right) && comparator(*left, pivot))
         left = std::next(left);
 
-      while ((left != right) && (*right >= pivot))
+      while ((left != right) && comparator(pivot, *right))
         right = std::prev(right);
 
       if (left != right)
@@ -30,29 +31,99 @@ namespace stiX {
     quick_sort(right, end);
   } // quick_sort
 
-  template<class Container>
-  void quick_sort(Container&& sample) {
+  template<class Container, class Comparator = std::less<>>
+  void quick_sort(Container&& sample, Comparator comparator = Comparator()) {
     quick_sort(std::begin(sample), std::end(sample));
   }
 } // namespace stiX
 
+std::string as_string(auto sample) {
+  auto out = std::ostringstream();
+  out << "{ ";
+
+  auto delim = "";
+  for (auto i : sample) {
+    out << delim << i;
+    delim = ", ";
+  }
+  out << " }";
+  return out.str();
+}
+
+auto const samples = std::vector<std::vector<int>> {
+  { },
+  { 1 },
+  { 1, 2 },
+  { 2, 1 },
+  { 1, 3, 2 },
+  { 3, 2, 1 },
+  { 5, 4, 1, 2, 3 }
+};
+
+auto const long_samples = std::vector<std::vector<int>> {
+  {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+  {3, 10, 9, 12, 8, 4, 1, 6, 5, 13, 2, 7, 11}
+};
+
+
+void test_quick_sort(auto sample) {
+  DYNAMIC_SECTION("sort " << as_string(sample)) {
+    auto expected = sample;
+    std::sort(std::begin(expected), std::end(expected));
+
+    auto under_test = sample;
+    stiX::quick_sort(under_test);
+
+    REQUIRE(under_test == expected);
+  }
+/*
+  DYNAMIC_SECTION("reverse sort " << as_string(sample)) {
+    auto expected = sample;
+    std::sort(std::begin(expected), std::end(expected), std::greater<>());
+
+    auto under_test = sample;
+    stiX::quick_sort(under_test, std::greater<>());
+
+    REQUIRE(under_test == expected);
+  }
+  */
+}
+
 TEST_CASE("Chapter 4 - quick sort") {
-  SECTION("empty list") {
-    auto sample = std::vector<int> { };
-    stiX::quick_sort(sample);
+  for (auto sample : samples)
+    test_quick_sort(sample);
+  for (auto sample : long_samples)
+    test_quick_sort(sample);
+
+  SECTION("sort a list") {
+    auto sample = std::list{5, 3, 1, 2, 3};
+    auto expected = sample;
+    expected.sort();
+
+    auto under_test = sample;
+    stiX::quick_sort(under_test);
+
+    REQUIRE(under_test == expected);
   }
 
-  SECTION("one item") {
-    auto sample = std::vector { 1 };
-    stiX::quick_sort(sample);
+  SECTION("sort a std::array") {
+    auto sample = std::array {5, 3, 1, 2, 3};
+    auto expected = sample;
+    std::sort(std::begin(expected), std::end(expected));
 
-    REQUIRE(sample == std::vector { 1 });
+    auto under_test = sample;
+    stiX::quick_sort(under_test);
+
+    REQUIRE(under_test == expected);
   }
 
-  SECTION("first partition") {
-    auto sample = std::vector { 4, 9, 1, 8, 2, 7 };
-    stiX::quick_sort(sample);
+  SECTION("sort an array") {
+    int under_test[6] = { 7, 3, 0, 8, 1, 2 };
 
-    REQUIRE(sample == std::vector { 1, 2, 4, 7, 8, 9 });
+    stiX::quick_sort(under_test);
+
+    int expected[6] = { 0, 1, 2, 3, 7, 8 };
+    for (auto i = 0; i != 6; ++i)
+      REQUIRE(under_test[i] == expected[i]);
   }
 }
