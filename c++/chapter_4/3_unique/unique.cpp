fd2eb17c3f2839579332f0fc080018fbb224ea39
Jez Higgins <jez@jezuk.co.uk>
2021-04-26 10:20:16 +0100

    slight tweak to unique

--- c++/chapter_4/3_unique/unique.cpp
+++ c++/chapter_4/3_unique/unique.cpp
@@ -6,12 +6,14 @@ auto const eof = std::char_traits<char>::eof();
 
 void stiX::unique(std::istream &in, std::ostream &out) {
   auto previous_line = std::string { 1, '\0' };
+
   while(in.peek() != eof) {
-    auto next_line = stiX::getline(in);
-    if (previous_line != next_line) {
-      out << next_line << "\n";
-      previous_line = next_line;
-    }
+    auto current_line = stiX::getline(in);
+
+    if (previous_line != current_line)
+      out << current_line << "\n";
+
+    previous_line = current_line;
   };
 }
 
