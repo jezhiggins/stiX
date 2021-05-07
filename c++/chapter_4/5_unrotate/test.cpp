4f46b4e2e5be5523263f573923aaed780aabc9c2
Jez Higgins <jez@jezuk.co.uk>
2021-05-08 00:14:59 +0100

    unrotate long lines too

--- c++/chapter_4/5_unrotate/test.cpp
+++ c++/chapter_4/5_unrotate/test.cpp
@@ -11,24 +11,30 @@ TEST_CASE("Chapter 4 - unrotate") {
   SECTION("marker at end") {
     testUnrotateLine(
       "This is a test.$",
-      "                 This is a test."
+      "                     This is a test."
     );
   }
   SECTION("marker after first word") {
     testUnrotateLine(
       "test.$This is a",
-      "      This is a  test."
+      "          This is a  test."
     );
   }
   SECTION("marker in middle") {
     testUnrotateLine(
       "a test.$This is",
-      "        This is  a test."
+      "            This is  a test."
+    );
+  }
+  SECTION("line is too long") {
+    testUnrotateLine(
+      "test.$Watch out! Much girthier",
+      " out! Much girthier  test.         Watch"
     );
   }
 }
 
 void testUnrotateLine(std::string const& input, std::string const& expected) {
-  auto result = stiX::unrotateLine(input, 32, '$');
+  auto result = stiX::unrotateLine(input, 40, '$');
   REQUIRE(result == expected);
 }
--- c++/chapter_4/5_unrotate/unrotate.cpp
+++ c++/chapter_4/5_unrotate/unrotate.cpp
@@ -5,12 +5,31 @@ std::string stiX::unrotateLine(std::string const& line, size_t line_length, char
   if (fold_position == std::string::npos)
     return line;
 
-  auto half_length = line_length / 2;
-  auto remainder = line.length() - fold_position;
+  auto half_length = (line_length / 2) - 1;
+
+  auto after_fold = line.substr(fold_position + 1);
+  auto before_fold = line.substr(0, fold_position);
+
+  auto post_padding = std::string();
+  auto wrapped = std::string();
+
+  if (after_fold.length() > half_length) {
+    auto wrap_at = after_fold.length() - half_length;
+    wrapped = after_fold.substr(0, wrap_at);
+    after_fold = after_fold.substr(wrap_at);
+    auto padding = before_fold.length() + wrap_at;
+    post_padding = std::string(half_length - padding, ' ');
+  }
+
+  auto lead_padding = std::string(half_length - after_fold.length(), ' ');
+
   auto output =
-    std::string(half_length - remainder, ' ') +
-    line.substr(fold_position + 1) +
+    lead_padding +
+    after_fold +
     "  " +
-    line.substr(0, fold_position);
-  return output;
+    before_fold +
+    post_padding +
+    wrapped;
+
+  return output.erase(output.find_last_not_of(' ') + 1);
 }
