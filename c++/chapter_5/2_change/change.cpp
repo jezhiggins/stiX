b799d03e7b596259640213204920f979ea528198
Jez Higgins <jez@jezuk.co.uk>
2021-12-10 16:05:44 +0000

    pull out body of change as its own function

--- c++/chapter_5/2_change/change.cpp
+++ c++/chapter_5/2_change/change.cpp
@@ -6,13 +6,15 @@
 
 auto const eof = std::char_traits<char>::eof();
 
-static bool is_zero_width_match(stiX::match_location loc) {
-  return loc.from == loc.to;
-}
+static bool is_zero_width_match(stiX::match_location loc);
+static bool not_at_end(int offset, std::string_view line);
 
-static bool not_at_end(int offset, std::string_view line) {
-  return offset != line.size();
-}
+static void apply_change(
+  stiX::pattern_matcher const& matcher,
+  std::string const& replacement,
+  std::string_view input,
+  std::ostream& out
+);
 
 void stiX::change(
   std::istream& in,
@@ -24,33 +26,49 @@ void stiX::change(
 
   while(in.peek() != eof) {
     auto input = stiX::getline(in);
-    auto line = std::string_view(input);
 
-    auto offset = 0;
-    auto last_match = -1;
-    while(not_at_end(offset, line)) {
-      auto loc = matcher.find(line, offset);
-      if (!loc.match)
-        break;
-
-      auto prefix = line.substr(offset, loc.from - offset);
-      if (last_match != loc.from)
-        out << prefix << replacement;
-
-      offset = loc.to;
-      last_match = loc.to;
-
-      if (is_zero_width_match(loc) && not_at_end(offset, line)) {
-        out << line[loc.from];
-        offset = loc.from+1;
-      }
-    }
-
-    if (not_at_end(offset, line))
-      out << line.substr(offset);
-    else if (!matcher.is_terminal_only() && matcher.find(line, line.size()).match)
-      out << replacement;
-
-    out << '\n';
+    apply_change(matcher, replacement, input, out);
   }
+}
+
+void apply_change(
+  stiX::pattern_matcher const& matcher,
+  std::string const& replacement,
+  std::string_view line,
+  std::ostream &out
+) {
+  auto offset = 0;
+  auto last_match = -1;
+  while(not_at_end(offset, line)) {
+    auto loc = matcher.find(line, offset);
+    if (!loc.match)
+      break;
+
+    auto prefix = line.substr(offset, loc.from - offset);
+    if (last_match != loc.from)
+      out << prefix << replacement;
+
+    offset = loc.to;
+    last_match = loc.to;
+
+    if (is_zero_width_match(loc) && not_at_end(offset, line)) {
+      out << line[loc.from];
+      offset = loc.from+1;
+    }
+  }
+
+  if (not_at_end(offset, line))
+    out << line.substr(offset);
+  else if (!matcher.is_terminal_only() && matcher.find(line, line.size()).match)
+    out << replacement;
+
+  out << '\n';
+}
+
+bool is_zero_width_match(stiX::match_location loc) {
+  return loc.from == loc.to;
+}
+
+bool not_at_end(int offset, std::string_view line) {
+  return offset != line.size();
 }
