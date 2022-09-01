ae086ddb6409cc83fb4de24e5d112827f37d47d8
Jez Higgins <jez@jezuk.co.uk>
2022-09-01 09:11:41 +0100

    pull out editor_test helper

--- c++/chapter_6/1_edit/test_editor.cpp
+++ c++/chapter_6/1_edit/test_editor.cpp
@@ -6,18 +6,37 @@
 #include <string>
 using namespace std::string_literals;
 
+void editor_test(std::string input, std::string expected);
+
 TEST_CASE("Chapter 6 - edit - editor") {
-  SECTION("new editor") {
-    auto input = "=\n"s;
-    auto output = "0\n"s;
+  editor_test(
+    "=\n"s,
+    "0\n"s
+  );
 
+  editor_test(
+    "=\n=\n"s,
+    "0\n0\n"s
+  );
+}
+
+std::string testLabel(std::string input);
+
+void editor_test(std::string input, std::string expected) {
+  DYNAMIC_SECTION("edit(" << testLabel(input) << ")") {
     auto e = stiX::editor();
+    auto is = std::istringstream(input);
+    auto output = std::ostringstream();
 
-    auto instr = std::istringstream(input);
-    auto ostr = std::ostringstream();
+    e.process(is, output);
 
-    e.process(instr, ostr);
-
-    REQUIRE(ostr.str() == output);
+    REQUIRE(output.str() == expected);
   }
+}
+
+std::string testLabel(std::string input) {
+  std::transform(input.begin(), input.end(), input.begin(), [](char ch) {
+    return ch == '\n' ? ' ' : ch;
+  });
+  return input;
 }
