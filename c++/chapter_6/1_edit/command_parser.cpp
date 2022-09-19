2c5042efa6816490b56f31c4fcbbe79a6b655dd7
Jez Higgins <jez@jezuk.co.uk>
2022-09-19 12:11:33 +0100

    use pattern matcher to isolate line numbers in command

--- c++/chapter_6/1_edit/command_parser.cpp
+++ c++/chapter_6/1_edit/command_parser.cpp
@@ -1,7 +1,20 @@
 #include "command_parser.hpp"
+#include "../../lib/regex/pattern_matcher.hpp"
+
+auto const line_numbers = stiX::compile_pattern("%[0-9\\.\\$+-,;]*");
+std::pair<size_t, size_t> parse_line_numbers(std::string_view number_input, size_t dot, size_t last);
 
 stiX::command stiX::parse_command(std::string_view input, size_t dot, size_t last) {
+  auto numbers = line_numbers.find(input);
+
+  auto [from, to] = parse_line_numbers(input.substr(numbers.from, numbers.to), dot, last);
+  input = input.substr(numbers.to);
+
   auto code = input.size() != 0 ? input.front() : '\n';
 
-  return command(dot, dot, code);
+  return command(from, to, code);
+}
+
+std::pair<size_t, size_t> parse_line_numbers(std::string_view number_input, size_t dot, size_t last) {
+  return std::make_pair(dot, dot);
 }
