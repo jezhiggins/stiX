636224c3b517ae1d6b2a06fbaf6873c3ee7de378
Jez Higgins <jez@jezuk.co.uk>
2022-10-11 08:53:42 +0100

    pop command_parser into anonymous namespace

--- c++/chapter_6/1_edit/command_parser.cpp
+++ c++/chapter_6/1_edit/command_parser.cpp
@@ -19,141 +19,147 @@ auto const DOLLAR = '$';
 auto const MINUS = '-';
 auto const PLUS = '+';
 
-class command_parser {
-public:
-  command_parser(std::string_view i, size_t d, size_t l):
-    input(i),
-    dot(d),
-    last(l)
-  {
-  }
+namespace {
 
-  stiX::command parse() {
-    parse_line_numbers();
-
-    parse_command_code();
-
-    return command();
-  }
-
-  stiX::command command() const {
-    if (is_error())
-      return stiX::command::error;
-
-    return { from, to, code };
-  }
-
-private:
-  void parse_command_code() {
-    code = !input.is_eol() ? input_pop() : '\n';
-
-    if (!input.is_eol())
-      failed();
-  }
-
-  void parse_line_numbers() {
-    from = to = parse_line_number();
-
-    if (is_separator())
-      input.advance();
-
-    if (is_index_start())
-      to = parse_line_number();
-  }
-
-  size_t parse_line_number() {
-    auto lhs = parse_index();
-
-    if (!is_operator())
-      return lhs;
-
-    auto op = input_pop();
-
-    auto rhs = parse_number();
-
-    if (op == MINUS)
-      rhs *= -1;
-
-    return lhs + rhs;
-  }
-
-  size_t parse_index() {
-    if (input.is_eol() || !is_index_start())
-      return dot;
-
-    switch(*input) {
-      case DOT:
-        input.advance();
-        return dot;
-      case DOLLAR:
-        input.advance();
-        return last;
-      case PLUS:
-      case MINUS:
-        return dot;
+  class command_parser {
+  public:
+    command_parser(std::string_view i, size_t d, size_t l) :
+      input(i),
+      dot(d),
+      last(l) {
     }
 
-    return parse_number();
-  }
+    stiX::command parse() {
+      parse_line_numbers();
 
-  size_t parse_number() {
-    auto n = std::string { };
-    while (!input.is_eol() && std::isdigit(*input))
-      n += input_pop();
+      parse_command_code();
 
-    auto num = stiX::command::line_error;
-    auto [_, ec] = std::from_chars(n.data(), n.data() + n.length(), num);
+      return command();
+    }
 
-    if (ec != std::errc())
-      failed();
+    stiX::command command() const {
+      if (is_error())
+        return stiX::command::error;
 
-    return num;
-  }
+      return {from, to, code};
+    }
 
-  bool is_index_start() {
-    auto c = *input;
-    return c == DOT ||
-      c == DOLLAR ||
-      c == MINUS ||
-      c == PLUS ||
-      std::isdigit(c);
-  }
-  bool is_separator() {
-    auto c = *input;
-    return c == ',' ||
-      c == ';';
-  }
-  bool is_operator() {
-    auto c = *input;
-    return c == PLUS || c == MINUS;
-  }
+  private:
+    void parse_command_code() {
+      code = !input.is_eol() ? input_pop() : '\n';
 
-  char input_pop() {
-    auto c = *input;
-    input.advance();
-    return c;
-  }
+      if (!input.is_eol())
+        failed();
+    }
 
-  bool is_error() const {
-    return has_failed ||
-      is_error(from) ||
-      is_error(to) ||
-      is_error(code);
-  }
-  static bool is_error(size_t f) { return f == stiX::command::line_error; }
-  static bool is_error(char c) { return c == stiX::command::code_error; }
+    void parse_line_numbers() {
+      from = to = parse_line_number();
 
-  void failed() { has_failed = true; }
+      if (is_separator())
+        input.advance();
 
-  stiX::character_sequence input;
-  size_t const dot;
-  size_t const last;
+      if (is_index_start())
+        to = parse_line_number();
+    }
 
-  size_t from = stiX::command::line_error;
-  size_t to = stiX::command::line_error;
-  char code = stiX::command::code_error;
-  bool has_failed = false;
-};
+    size_t parse_line_number() {
+      auto lhs = parse_index();
+
+      if (!is_operator())
+        return lhs;
+
+      auto op = input_pop();
+
+      auto rhs = parse_number();
+
+      if (op == MINUS)
+        rhs *= -1;
+
+      return lhs + rhs;
+    }
+
+    size_t parse_index() {
+      if (input.is_eol() || !is_index_start())
+        return dot;
+
+      switch (*input) {
+        case DOT:
+          input.advance();
+          return dot;
+        case DOLLAR:
+          input.advance();
+          return last;
+        case PLUS:
+        case MINUS:
+          return dot;
+      }
+
+      return parse_number();
+    }
+
+    size_t parse_number() {
+      auto n = std::string{};
+      while (!input.is_eol() && std::isdigit(*input))
+        n += input_pop();
+
+      auto num = stiX::command::line_error;
+      auto [_, ec] = std::from_chars(n.data(), n.data() + n.length(), num);
+
+      if (ec != std::errc())
+        failed();
+
+      return num;
+    }
+
+    bool is_index_start() {
+      auto c = *input;
+      return c == DOT ||
+             c == DOLLAR ||
+             c == MINUS ||
+             c == PLUS ||
+             std::isdigit(c);
+    }
+
+    bool is_separator() {
+      auto c = *input;
+      return c == ',' ||
+             c == ';';
+    }
+
+    bool is_operator() {
+      auto c = *input;
+      return c == PLUS || c == MINUS;
+    }
+
+    char input_pop() {
+      auto c = *input;
+      input.advance();
+      return c;
+    }
+
+    bool is_error() const {
+      return has_failed ||
+             is_error(from) ||
+             is_error(to) ||
+             is_error(code);
+    }
+
+    static bool is_error(size_t f) { return f == stiX::command::line_error; }
+
+    static bool is_error(char c) { return c == stiX::command::code_error; }
+
+    void failed() { has_failed = true; }
+
+    stiX::character_sequence input;
+    size_t const dot;
+    size_t const last;
+
+    size_t from = stiX::command::line_error;
+    size_t to = stiX::command::line_error;
+    char code = stiX::command::code_error;
+    bool has_failed = false;
+  };
+} // namespace
 
 stiX::command stiX::parse_command(std::string_view input, size_t dot, size_t last) {
   auto parser = command_parser(input, dot, last);
