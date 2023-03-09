c10942d8d8fe3894ea21b95c9af98c46c828da77
Jez Higgins <jez@jezuk.co.uk>
2023-03-09 22:24:45 +0000

    confirmatory index underflow and overflow tests

--- c++/chapter_6/1_edit/test_command_parser.cpp
+++ c++/chapter_6/1_edit/test_command_parser.cpp
@@ -55,12 +55,18 @@ namespace {
   };
 
   auto bad_test_cases = std::vector<parse_test_case>{
-    {"no hex please",       {"1a,3=", 5, 10}},
-    {"dot on rhs of -",     {"5-.",   5, 10}},
-    {"dollar on rhs of -",  {"15-$",  5, 10}},
-    {"$-.",                 {"$-.",   5, 10}},
-    {"from index too large",{ "99",   5, 10}},
-    {"to index too large",  { "1,99", 5, 10}}
+    {"no hex please",         {"1a,3=", 5, 10}},
+    {"dot on rhs of -",       {"5-.",   5, 10}},
+    {"dollar on rhs of -",    {"15-$",  5, 10}},
+    {"$-.",                   {"$-.",   5, 10}},
+    {"from index too large",  { "99",   5, 10}},
+    {"to index too large",    { "1,99", 5, 10}},
+    {"arithmetic underflow",  { "-20",  5, 10}},
+    {". arithmetic underflow",{ ".-20", 5, 10}},
+    {"$ arithmetic underflow",{ "$-20", 5, 10}},
+    {"arithmetic overflow",   { "+20",  5, 10}},
+    {". arithmetic overflow", { ".+20", 5, 10}},
+    {"$ arithmetic overflow", { "$+20", 5, 10}}
   };
 }
 
