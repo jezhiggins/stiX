167145e0f95299cff6ca483e57ce8e06f4c64d13
Jez Higgins <jez@jezuk.co.uk>
2023-09-20 20:52:52 +0100

    some additional line_buffer.insert characterisation tests

--- c++/chapter_6/1_edit/test_line_buffer.cpp
+++ c++/chapter_6/1_edit/test_line_buffer.cpp
@@ -37,4 +37,28 @@ TEST_CASE("Chapter 6 - edit - line_buffer") {
     REQUIRE(b[0] == "line 1");
     REQUIRE(b[1] == "line 2");
   }
+
+  SECTION("insert a line before end, then at the start, then at the end") {
+    auto b = stiX::line_buffer();
+    b.insert(b.size(), "line 1");
+    b.insert(0, "line 2");
+    b.insert(b.size(), "line 3");
+
+    REQUIRE(b.size() == 3);
+    REQUIRE(b[0] == "line 2");
+    REQUIRE(b[1] == "line 1");
+    REQUIRE(b[2] == "line 3");
+  }
+
+  SECTION("insert a line before end, then at the start, then before the second line") {
+    auto b = stiX::line_buffer();
+    b.insert(b.size(), "line 1");
+    b.insert(0, "line 2");
+    b.insert(1, "line 3");
+
+    REQUIRE(b.size() == 3);
+    REQUIRE(b[0] == "line 2");
+    REQUIRE(b[1] == "line 3");
+    REQUIRE(b[2] == "line 1");
+  }
 }
