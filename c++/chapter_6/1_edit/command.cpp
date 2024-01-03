4f15b864cabeef9dd1d2ace231585511b4cca190
Jez Higgins <jez@jezuk.co.uk>
2024-01-03 15:24:06 +0000

    edit: set line_error properly - more properly anyway

--- c++/chapter_6/1_edit/command.cpp
+++ c++/chapter_6/1_edit/command.cpp
@@ -1,7 +1,10 @@
 #include "command.hpp"
+
+#include <limits>
+
 #include "edit_buffer.hpp"
 
-size_t const stiX::command::line_error = -1;
+size_t const stiX::command::line_error = std::numeric_limits<size_t>::max();
 char const stiX::command::code_error = '?';
 
 void stiX::command::operator()(
