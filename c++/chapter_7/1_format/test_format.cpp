e921da668c62986446364cbd809245aebcd0288a
Jez Higgins <jez@jezuk.co.uk>
2024-06-15 21:44:23 +0100

    format: rework tests

--- c++/chapter_7/1_format/test_format.cpp
+++ c++/chapter_7/1_format/test_format.cpp
@@ -2,30 +2,26 @@
 
 #include "format.hpp"
 
+void format_test(std::string input, std::string expected);
+
 TEST_CASE("unformatted text") {
   SECTION("short line") {
-    auto in = std::istringstream { };
-    in.str("hello\n");
-
-    auto out = std::ostringstream { };
-    auto formatter = stiX::formatter { };
-
-    formatter(in, out);
-
-    auto output = out.str();
-    REQUIRE(output == "hello\n");
+    format_test("hello\n", "hello\n");
   }
 
   SECTION("two short lines, one short output line") {
-    auto in = std::istringstream { };
-    in.str("hello\nworld\n");
-
-    auto out = std::ostringstream { };
-    auto formatter = stiX::formatter { };
-
-    formatter(in, out);
-
-    auto output = out.str();
-    REQUIRE(output == "hello world\n");
+    format_test("hello\nworld\n", "hello world\n");
   }
+}
+
+void format_test(std::string input, std::string expected) {
+  auto in = std::istringstream { };
+  in.str(input);
+
+  auto out = std::ostringstream { };
+  auto formatter = stiX::formatter { };
+  formatter(in, out);
+
+  auto output = out.str();
+  REQUIRE(output == expected);
 }
