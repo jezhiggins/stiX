f1678ed093bb63ba365ef690aedce246ce5a1938
Jez Higgins <jez@jezuk.co.uk>
2024-06-30 23:16:29 +0100

    format: more underline cleanup

--- c++/chapter_7/1_format/format_functions.cpp
+++ c++/chapter_7/1_format/format_functions.cpp
@@ -43,8 +43,8 @@ std::string stiX::centre_line(std::string const& line_in, size_t width) {
 }
 
 namespace {
-  constinit std::string start_underline = "\x1B[4m";
-  constinit std::string end_underline = "\x1B[0m";
+  constinit std::string start_underline_control = "\x1B[4m";
+  constinit std::string end_underline_control = "\x1B[0m";
 
   size_t find_boundary(std::string const& line, size_t from, auto predicate) {
     while (predicate(line[from]) && from != line.size())
@@ -57,6 +57,20 @@ namespace {
   size_t word_end(std::string const& line, size_t from) {
     return find_boundary(line, from, std::not_fn(isspace));
   }
+
+  void insert_underline_control(
+    std::string& line,
+    size_t& boundary,
+    std::string const& control) {
+    line.insert(boundary, control);
+    boundary += control.size();
+  }
+  void begin_underlining(std::string& line, size_t& boundary) {
+    insert_underline_control(line, boundary, start_underline_control);
+  }
+  void end_underlining(std::string& line, size_t& boundary) {
+    insert_underline_control(line, boundary, end_underline_control);
+  }
 }
 
 std::string stiX::underline(std::string_view line_in) {
@@ -64,12 +78,10 @@ std::string stiX::underline(std::string_view line_in) {
 
   auto boundary = word_start(line, 0);
   while (boundary != line.size()) {
-    line.insert(boundary, start_underline);
-    boundary += start_underline.size();
+    begin_underlining(line, boundary);
 
     boundary = word_end(line, boundary);
-    line.insert(boundary, end_underline);
-    boundary += end_underline.size();
+    end_underlining(line, boundary);
 
     boundary = word_start(line, boundary);
   }
