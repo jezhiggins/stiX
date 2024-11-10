508805b673f48211a0944ea13547fb486f14412c
Jez Higgins <jez@jezuk.co.uk>
2024-11-10 21:21:08 +0000

    macro: pull out all_to_string/seq

--- c++/chapter_8/1_macro/macro.cpp
+++ c++/chapter_8/1_macro/macro.cpp
@@ -38,6 +38,9 @@ namespace {
     std::string sub_frame_to_string(token_seq const& in);
     token_seq sub_frame_to_seq(token_seq const& in);
 
+    std::vector<std::string> all_to_string(std::vector<token_seq> const&& args);
+    std::vector<token_seq> all_to_seq(std::vector<token_seq> const&& args);
+
     void define_replacement(std::string const&, token_stream&, token_sink&);
     void len_macro(std::string const&, token_stream&, token_sink&);
     void ifelse_macro(std::string const&, token_stream&, token_sink&);
@@ -150,6 +153,18 @@ namespace {
     return sink;
   }
 
+  std::vector<std::string> macro_processor::all_to_string(std::vector<token_seq> const&& args) {
+    return args
+      | std::views::transform([this](token_seq const& a) { return sub_frame_to_string(a); })
+      | std::ranges::to<std::vector>();
+  }
+
+  std::vector<token_seq> macro_processor::all_to_seq(std::vector<token_seq>const&& args) {
+    return args
+      | std::views::transform([this](token_seq const& a) { return sub_frame_to_seq(a); })
+      | std::ranges::to<std::vector>();
+  }
+
   void macro_processor::define_replacement(
       std::string const& macro,
       token_stream& source,
@@ -207,9 +222,7 @@ namespace {
     if (raw_arguments.size() < 3)
       return;
 
-    auto arguments = raw_arguments
-                     | std::views::transform([this](token_seq const& a) { return sub_frame_to_string(a); })
-                     | std::ranges::to<std::vector>();
+    auto arguments = all_to_string(std::move(raw_arguments));
 
     auto const& lhs = arguments[0];
     auto const& rhs = arguments[1];
@@ -232,9 +245,7 @@ namespace {
     warning_if_excess(macro, raw_arguments, 3);
     warning_if_too_few(macro, raw_arguments, 2);
 
-    auto arguments = raw_arguments
-                     | std::views::transform([this](token_seq const& a) { return sub_frame_to_string(a); })
-                     | std::ranges::to<std::vector>();
+    auto arguments = all_to_string(std::move(raw_arguments));
     auto const& str = !arguments.empty() ? arguments[0] : Empty;
     auto [start, start_ok] = int_arg(arguments, 1);
     auto [len, len_ok] = int_arg(arguments, 2, static_cast<int>(std::string::npos));
@@ -263,9 +274,7 @@ namespace {
     token_stream& source,
     token_sink&
   ) {
-    auto arguments = gather_arguments(source)
-      | std::views::transform([this](token_seq const& a) { return sub_frame_to_seq(a); })
-      | std::ranges::to<std::vector>();
+    auto arguments = all_to_seq(gather_arguments(source));
 
     auto definition = token_stream { replacements_[token] };
 
